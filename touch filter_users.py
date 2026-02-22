ce91ad4163eaf842260f6f306600cc969d27473c
angemare <a.stragies.com>
2026-02-22 21:25:02 +0100

    add age to .py

--- touch filter_users.py
+++ touch filter_users.py
@@ -5,7 +5,11 @@ def filter_users_by_name(name):
     with open("users.json", "r") as file:
         users = json.load(file)
 
-    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
+def filter_users_by_age(age):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if user["name"].lower() == name.lower()], [user for user in users if user["age"].lower() == age.lower()]
 
     for user in filtered_users:
         print(user)
@@ -17,5 +21,10 @@ if __name__ == "__main__":
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+        print(name_to_search)
+    elif filter_option == "age":
+        age_to_search = input("Enter an age to filter users: ").strip()
+        filter_users_by_age(age_to_search)
+        print(age_to_search)
     else:
         print("Filtering by that option is not yet supported.")
