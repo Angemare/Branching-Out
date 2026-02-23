8655cd37af175db465dc3b672f6abfa09cb18c42
angemare <a.stragies.com>
2026-02-23 10:22:25 +0100

    solve errors

--- touch filter_users.py
+++ touch filter_users.py
@@ -5,19 +5,34 @@ def filter_users_by_input(name):
     with open("users.json", "r") as file:
         users = json.load(file)
 
-#def filter_users_by_age(age):
-    with open("users.json", "r") as file:
-        users = json.load(file)
-
-#def filter_users_by_email(email):
-    with open("users.json", "r") as file:
-        users = json.load(file)
-
-    filtered_users = [user for user in users if user["name"].lower() == name.lower()], [user for user in users if user["age"].lower() == age.lower()],[user for user in users if user["email"].lower() == email.lower()]
+    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
 
     for user in filtered_users:
         print(user)
+    if not filtered_users:
+        print("No user found")
 
+def filter_users_by_age(age):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if user["age"].lower() == age.lower()]
+
+    for user in filtered_users:
+        print(user)
+    if not filtered_users:
+        print("User not found")
+
+def filter_users_by_email(email):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if user["email"].lower() == email.lower()]
+
+    for user in filtered_users:
+        print(user)
+    if not filtered_users:
+        print("User not found")
 
 if __name__ == "__main__":
     filter_option = input("What would you like to filter by? (Currently, only 'name', 'age' and 'email' is supported): ").strip().lower()
@@ -31,8 +46,10 @@ if __name__ == "__main__":
         filter_users_by_input(age_to_search)
         print(age_to_search)
     elif filter_option == "email":
-        email_to_search = input("Enter an Email to filter users: ")
+        email_to_search = input("Enter an Email to filter users: ").strip()
         filter_users_by_input(email_to_search)
         print(email_to_search)
     else:
         print("Filtering by that option is not yet supported.")
+
+
